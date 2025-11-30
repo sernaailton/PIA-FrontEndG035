ab6927f84dfa46f1058346f32f762fc4e47d85a6
sernaailton <ailtonserna64@gmail.com>
2025-11-30 07:35:57 -0600

    Navbar responsiva con navegacion interna (ya con interactividad funcional)

--- sernamotors.js
+++ sernamotors.js
@@ -1,1 +1,26 @@
-// Interactividad se agregará en commits posteriores
+// menu mobile
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+
+  // ARIA accesibilidad
+  const abierto = navLinks.classList.contains("show");
+  menuToggle.setAttribute("aria-expanded", abierto);
+});
+
+// Scroll para navegación interna
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const destino = document.querySelector(link.getAttribute("href"));
+    destino.scrollIntoView({
+      behavior: "smooth"
+    });
+
+    navLinks.classList.remove("show");
+    menuToggle.setAttribute("aria-expanded", "false");
+  });
+});
